f14bd0c372590411d1958647cdba2889220243f4
Rahulshahare <rahulshahare@gmail.com>
2020-06-27 17:50:24 +0530

    The expression i ~/ 2 divides i by 2 and returns an integer result.

--- lib/main.dart
+++ lib/main.dart
@@ -42,7 +42,7 @@ class _RandomWordsState extends State<RandomWords> {
       itemBuilder: /*1 */(context,i){
         if(i.isOdd) return Divider();  /**2 */
 
-        final index = i ~/ 2; 
+        final index = i ~/ 2; /**3 */
         if (index >= _suggestions.length){
           _suggestions.addAll(generateWordPairs().take(10));
         }
