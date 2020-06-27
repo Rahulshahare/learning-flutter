1eee901b4e4a7a290b2243a7d02e283408e8e614
Rahulshahare <rahulshahare@gmail.com>
2020-06-27 17:22:59 +0530

    The Big Change. the build function is updated

--- lib/main.dart
+++ lib/main.dart
@@ -10,17 +10,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     // final wordPair = WordPair.random();
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          // child: Text('Hello World'),
-          // child: Text(wordPair.asPascalCase),
-          child: RandomWords(),
-        ),
-      ),
+      title: 'Startup Name Generator',
+      home: RandomWords(),
     );
   }
 }
